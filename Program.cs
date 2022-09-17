678b559401d7ca0b7eb5a5f346c5298a5db01281
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-09-17 23:44:30 +0300

    KArar Yapıları Switch

--- Program.cs
+++ Program.cs
@@ -263,3 +263,81 @@ sonuç = time >= 6 && time < 11 ? "Günaydın." : time <= 18 ? "İyi günler." :
 Console.WriteLine(sonuç);
 #endregion
 
+#region Karar Yapıları -Switch-Case
+int month = DateTime.Now.Month;
+
+switch (month) //Kontrol etmek istediğimiz koşulu yazarız.
+{
+    case 1:
+        Console.WriteLine("Ocak ayındasınız.");
+        break;
+    case 2:
+        Console.WriteLine("Şubat ayındasınız.");
+        break;
+    case 3:
+        Console.WriteLine("Mart ayındasınız.");
+        break;
+    case 4:
+        Console.WriteLine("Nisan ayındasınız.");
+        break;
+    case 5:
+        Console.WriteLine("Mayıs ayındasınız.");
+        break;
+    case 6:
+        Console.WriteLine("Haziran ayındasınız.");
+        break;
+    case 7:
+        Console.WriteLine("Temmuz ayındasınız.");
+        break;
+    case 8:
+        Console.WriteLine("Ağustos ayındasınız.");
+        break;
+    case 9:
+        Console.WriteLine("Eylül ayındasınız.");
+        break;
+    case 10:
+        Console.WriteLine("Ekim ayındasınız.");
+        break;
+    case 11:
+        Console.WriteLine("Kasım ayındasınız.");
+        break;
+    case 12:
+        Console.WriteLine("Aralık ayındasınız.");
+        break;
+
+    default: //Hiçbir case olmazsa yapılacak olan işlemi ifade eder.
+        Console.WriteLine("Yanlış veri girdiniz.");
+        break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış ayındasınız.");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Bahar ayındasınız.");
+        break;
+
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz ayındasınız.");
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonbahar ayındasınız.");
+        break;
+
+    default:
+        break;
+}
+
+#endregion
